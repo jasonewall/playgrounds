5a787349458c6bd42c2a258f9d32024559611f73
Jason Wall <javajo@gmail.com>
2023-09-11 04:17:22 +0000

    styling

--- react-ts/src/components/ProductList.tsx
+++ react-ts/src/components/ProductList.tsx
@@ -4,12 +4,12 @@ import ProductCard from "./ProductCard";
 function ProductList() {
     const { products } = useProducts();
 
-    return  <>
+    return  <div className="ml-20 mr-20">
         <h1 className="text-center text-4xl font-extrabold">Products</h1>
         <div className="grid grid-cols-4 gap-4">
             {products.map(product => (<ProductCard key={product.id} product={product} />))}
         </div>
-    </>
+    </div>
 }
 
 export default ProductList;
