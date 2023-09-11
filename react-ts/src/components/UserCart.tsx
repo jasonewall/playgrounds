0f977c2a898dd95a5b866a4bd1fc65086dc5be97
Jason Wall <javajo@gmail.com>
2023-09-11 03:16:02 +0000

    style cart a bit

--- react-ts/src/components/UserCart.tsx
+++ react-ts/src/components/UserCart.tsx
@@ -2,13 +2,14 @@ import useCart from "@entities/useCart";
 
 function UserCart() {
     const { lineItems } = useCart();
-    return <>
-        <ul>
+    return <div className="ml-20 mr-20">
+        <h1 className="text-center text-4xl font-extrabold">Your Cart</h1>
+        <ul className="list-disc list-outsdide">
             {lineItems.map(lineItem => (
                 <li key={lineItem.product.id}>{lineItem.product.name} - {lineItem.quantity}</li>
             ))}
         </ul>
-    </>
+    </div>
 }
 
 export default UserCart;
